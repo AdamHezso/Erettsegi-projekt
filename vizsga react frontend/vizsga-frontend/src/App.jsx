9b500b23a3987640fe9593b0a3f4fbf9120f0bd7
Hezso Adam <hezso.adam19k@egressy.info>
2025-04-20 15:44:17 +0200

    reactV2 uristen dejoo

--- vizsga react frontend/vizsga-frontend/src/App.jsx
+++ vizsga react frontend/vizsga-frontend/src/App.jsx
@@ -1,30 +1,14 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { CartProvider } from './context/CartContext';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ProductList from './pages/ProductList';
-import CartPage from './pages/CartPage';
-import Checkout from './pages/Checkout';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import NavigationBar from './components/navbar';
 
 function App() {
     return (
-        <BrowserRouter>
-            <CartProvider>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<ProductList />} />
-                    <Route path="/cart" element={<CartPage />} />
-                    <Route path="/checkout" element={<Checkout />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                </Routes>
-                <Footer />
-            </CartProvider>
-        </BrowserRouter>
+        <div className="App">
+            <h1>Welcome to the Auto Parts Shop</h1>
+            <p>Your one-stop shop for all your auto parts needs!</p>
+            <NavigationBar>asd</NavigationBar>
+        </div>
     );
 }
 
